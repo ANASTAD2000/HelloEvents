5d35e75ed7bc3ae1c656d12ce64dd42210cc080d
ANASTAD2000 <anasbaclibr@gmai.com>
2025-06-03 13:50:31 +0100

    initial commit : setup spring boot backend  and angular frontend structur

---
+++ helloevents-backend/src/main/java/com/helloevents/helloevents_backend/HelloeventsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.helloevents.helloevents_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloeventsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloeventsBackendApplication.class, args);
+	}
+
+}
---
+++ helloevents-backend/src/test/java/com/helloevents/helloevents_backend/HelloeventsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloevents.helloevents_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloeventsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
